8cfaaebfd7325d16570c7442f35b42470c50c786
lin826 <liniju826@gmail.com>
2020-02-14 10:05:59 +0000

    Split header and footer as views

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    勤能補拙，每日精進
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >關於</a>
+  </div>
+</footer>
