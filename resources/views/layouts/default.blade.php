37aaba847b4d316840ada3bcfb4a8e32ebfa90b7
lin826 <liniju826@gmail.com>
2020-02-14 08:05:34 +0000

    Init style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel Beginner Project</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">Get Help</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>Index Page</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      現在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入門教學</a> 的範例專案首頁。
+    </p>
+    <p>
+      一切，將從這裡開始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">現在註冊</a>
+    </p>
+  </div>
 @stop
