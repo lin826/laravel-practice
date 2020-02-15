704859436730e9741855cc95e421efe4ea855d33
lin826 <liniju826@gmail.com>
2020-02-15 10:02:03 +0000

    Update register page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'Register')
 
 @section('content')
-<h1>Register</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>Register</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="name">使用者名稱：</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">電子信箱：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">密碼設定：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">再次輸入密碼：</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">註冊</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
