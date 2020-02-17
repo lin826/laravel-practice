bcee0da278d327121c1fab8ae06e1fea2833482e
lin826 <liniju826@gmail.com>
2020-02-17 23:42:02 +0800

    Add password reset

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.default')
+@section('title', 'Reset Password')
+
+@section('content')
+<div class="col-md-8 offset-md-2">
+  <div class="card ">
+    <div class="card-header"><h5>Reset Password</h5></div>
+
+    <div class="card-body">
+      @if (session('status'))
+      <div class="alert alert-success">
+        {{ session('status') }}
+      </div>
+      @endif
+
+      <form class="" method="POST" action="{{ route('password.email') }}">
+        {{ csrf_field() }}
+
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+          <label for="email" class="form-control-label">E-mail:</label>
+
+          <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+          @if ($errors->has('email'))
+            <span class="form-text">
+              <strong>{{ $errors->first('email') }}</strong>
+            </span>
+          @endif
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">
+            Send e-mail to reset password
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
+
+————————————————
+Author: Summer
+Reference: https://learnku.com/courses/laravel-essential-training/6.x/password-reset/5490
